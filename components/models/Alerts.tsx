1b65bebfd1963c1e562844f9eff2fc619bf92a87
Etienne BLANC <etienne.blanc94@gmail.com>
2020-07-02 21:39:09 +0200

    add shared props

--- components/models/Alerts.tsx
+++ components/models/Alerts.tsx
@@ -1,23 +1,16 @@
-import { styled } from "styles";
+import { styled, css } from "styles";
 
+const sharedProps = css.compose(
+  css.border(2),
+  css.rounded("md"),
+  css.p(1),
+  css.italic(),
+  css.text("sm")
+);
 export const InfoAlert = styled.div((css) =>
-  css.compose(
-    css.border(2),
-    css.rounded("md"),
-    css.p(1),
-    css.italic(),
-    css.text("sm"),
-    css.bg("green-200")
-  )
+  css.compose(sharedProps, css.bg("green-200"))
 );
 
 export const DangerAlert = styled.div((css) =>
-  css.compose(
-    css.border(2),
-    css.rounded("md"),
-    css.p(1),
-    css.italic(),
-    css.text("sm"),
-    css.bg("red-400")
-  )
+  css.compose(sharedProps, css.bg("red-400"))
 );
